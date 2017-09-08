055b50bb35f3e69ddc5a9c7741000e8326a9aa59
Justinas Marcinka <jumar@dbml.dk>
2017-09-08 22:57:21 +0300

    Implement subtract operation

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function sum(a, b) {
 	return a + b;
 }
 
+function sub(a, b) {
+	return a - b;
+}
+
 function main() {
 	const operand1 = parseInt(process.argv[2]);
 	const operation = process.argv[3];
@@ -10,6 +14,9 @@ function main() {
 	switch(operation) {
 		case '+':
 			console.log(sum(operand1, operand2));
+			break;
+		case '-':
+			console.log(sub(operand1, operand2));
 			break;
 		default:
 			console.error(`Operation ${operation} is not supported`);
