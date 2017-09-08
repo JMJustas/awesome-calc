38da962f1e81a1e03fb68131ace17158b94fedcf
Justinas Marcinka <jumar@dbml.dk>
2017-09-08 22:38:49 +0300

    Implement argument parsing from cli

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 function main() {
-	console.log('Hello world!');
+	const operand1 = process.argv[2];
+	const operation = process.argv[3];
+	const operand2 = process.argv[4];
+	console.log(operand1, operation, operand2);
 }
 
 main()
